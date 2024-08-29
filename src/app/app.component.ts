0310437b493c998f695d9daf37d5568819ca2f12
karolkolodziej <karolkolodziej01@gmail.com>
2024-08-29 12:39:25 +0200

    Add chart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, Inject, PLATFORM_ID } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import Highcharts from "highcharts/es-modules/masters/highcharts.src";
+import { HighchartsChartModule } from "highcharts-angular";
+import { isPlatformBrowser, isPlatformServer } from "@angular/common";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: "app-root",
+	standalone: true,
+	imports: [RouterOutlet, HighchartsChartModule],
+	templateUrl: "./app.component.html",
+	styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'hc-ssr';
+	public Highcharts: typeof Highcharts = Highcharts;
+	public chartOptions: Highcharts.Options = {
+		series: [
+			{
+				data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+				type: "line",
+			},
+		],
+	};
+    public isBrowser: boolean;
+    public isServer: boolean;
+
+    constructor(@Inject(PLATFORM_ID) platformId: Object) {
+      this.isBrowser = isPlatformBrowser(platformId);
+      this.isServer = isPlatformServer(platformId);
+
+    }
 }
